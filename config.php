0a410df9b3128d7d9ef90cbc583c9289499b5780
robotcn1130 <robinchina@vip.qq.com>
2026-02-28 16:22:28 +0800

    添加 config.php 配置文件

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// 数据库配置
+define('DB_HOST', 'localhost');
+define('DB_PORT', '3306');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'gp');
+
+// 系统配置
+define('SYSTEM_INSTALLED', false);
